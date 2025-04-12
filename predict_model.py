51d6f35dda07b981d5a985ccdb1cb21c49854fc6
Phillip <philip.ogn@gmail.com>
2025-04-12 23:27:14 +0100

    refactoring + loading model and tokenizer now in functions

--- predict_model.py
+++ predict_model.py
@@ -9,20 +9,18 @@ from utils import analyze_prompt
 
 DATASET_TEST = [
     # "data/test/deepset_test.csv",
-    "data/test/promptshield_dataset.csv",
+    # "data/test/promptshield_dataset.csv",
     # "data/test/jackhhao_jailbreak_dataset.csv",
-    # "data/test/wild_awesome_gen_test.csv"
+    # "data/test/wild_awesome_gen_test.csv",
+    "data/test/qualifire.csv"
 ]
 
-MODEL_NAME = "meta-llama/Llama-3.2-1B"
-# MODEL_NAME = "Qwen/Qwen2.5-1.5B-Instruct"
-# MODEL_NAME = "microsoft/Phi-3-mini-128k-instruct"
-
-# MAYBE PUT THIS INTO A FUNCTION???
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForCausalLM.from_pretrained(
-    MODEL_NAME, output_attentions=True, return_dict_in_generate=True, attn_implementation="eager", device_map="auto"
-)
+def load_model_and_tokenizer(model_name):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, output_attentions=True, return_dict_in_generate=True, attn_implementation="eager", device_map="auto"
+    )
+    return tokenizer, model
 
 def load_datasets(dataset_paths):
     dataframes = []
@@ -41,7 +39,7 @@ def load_datasets(dataset_paths):
     
     return pd.concat(dataframes, ignore_index=True)
 
-def predict_prompt_logreg(prompts, saved_model_name):
+def predict_prompt_logreg(prompts, saved_model_name, tokenizer, model):
     predictions = []
     with open(f'saved_models/{saved_model_name}_log_model.pkl', 'rb') as file:
         loaded_model, loaded_tfidf, loaded_scaler = load(file)
@@ -55,7 +53,7 @@ def predict_prompt_logreg(prompts, saved_model_name):
         predictions.append(prediction[0])
     return predictions
 
-def predict_prompt_svm(prompts, saved_model_name):
+def predict_prompt_svm(prompts, saved_model_name, tokenizer, model):
     predictions = []
     with open(f'saved_models/{saved_model_name}_svm_model.pkl', 'rb') as file:
         loaded_model, loaded_tfidf, loaded_scaler = load(file)
@@ -69,7 +67,7 @@ def predict_prompt_svm(prompts, saved_model_name):
         predictions.append(prediction[0])
     return predictions
 
-def predict_prompt_randforest(prompts, saved_model_name):
+def predict_prompt_randforest(prompts, saved_model_name, tokenizer, model):
     predictions = []
     with open(f'saved_models/{saved_model_name}_rand_model.pkl', 'rb') as file:
         loaded_model, loaded_tfidf = load(file)
@@ -82,40 +80,43 @@ def predict_prompt_randforest(prompts, saved_model_name):
         predictions.append(prediction[0])
     return predictions
 
-def evaluation_metrics(true_labels, predicted_labels, model_type):
+def evaluation_metrics(true_labels, predicted_labels, model_name, classifier_type):
     # calculate metrics and print
     precision_unseen = metrics.precision_score(true_labels, predicted_labels)
     recall_unseen = metrics.recall_score(true_labels, predicted_labels)
     f1_unseen = metrics.f1_score(true_labels, predicted_labels)
     accuracy = accuracy_score(true_labels, predicted_labels)
     
-    print(f"\nPredicting on unseen data with {MODEL_NAME} ({model_type})")
+    print(f"\nPredicting on unseen data with {model_name} ({classifier_type})")
     print(f"Precision: {precision_unseen} | Recall: {recall_unseen} | F1: {f1_unseen}")
     print(f"Model Accuracy: {accuracy:.4f}")
     print("Classification Report: ", classification_report(true_labels, predicted_labels))
 
 
 if __name__ == "__main__":
-    df = load_datasets(DATASET_TEST)
-    prompts = df["prompt"].tolist()
-    true_labels = df["label"].astype(int).tolist()
+    selected_model = "llama"  # change to load saved language models: llama, qwen, phi
 
-    models = {
-        "llama": ("meta-llama/Llama-3.2-1B",),
-        "qwen": ("Qwen/Qwen2.5-1.5B-Instruct",),
-        "phi": ("microsoft/Phi-3-mini-128k-instruct")
+    model_options = {
+        "llama": "meta-llama/Llama-3.2-1B",
+        "qwen": "Qwen/Qwen2.5-1.5B-Instruct",
+        "phi": "microsoft/Phi-3-mini-128k-instruct"
     }
+
+    model_name = model_options[selected_model]
+    tokenizer, model = load_model_and_tokenizer(model_name)
     
-    saved_model_name = "llama"  # change to load saved language models: llama, qwen, phi
+    load_df = load_datasets(DATASET_TEST)
+    prompts = load_df["prompt"].tolist()
+    true_labels = load_df["label"].astype(int).tolist()
 
     # logistic regression
-    predicted_labels = predict_prompt_logreg(prompts, saved_model_name)
-    evaluation_metrics(true_labels, predicted_labels, "LOGISTIC REGRESSION")
+    predicted_labels = predict_prompt_logreg(prompts, selected_model, tokenizer, model)
+    evaluation_metrics(true_labels, predicted_labels, model_name, "LOGISTIC REGRESSION")
 
     # svm
-    predicted_labels_svm = predict_prompt_svm(prompts, saved_model_name)
-    evaluation_metrics(true_labels, predicted_labels_svm, "SVM")
+    predicted_labels_svm = predict_prompt_svm(prompts, selected_model, tokenizer, model)
+    evaluation_metrics(true_labels, predicted_labels_svm, model_name, "SVM")
 
     # random forest
-    predicted_labels_rand = predict_prompt_randforest(prompts, saved_model_name)
-    evaluation_metrics(true_labels, predicted_labels_rand, "RANDOM FOREST")
+    predicted_labels_rand = predict_prompt_randforest(prompts, selected_model, tokenizer, model)
+    evaluation_metrics(true_labels, predicted_labels_rand, model_name, "RANDOM FOREST")
--- train_model.py
+++ train_model.py
@@ -21,14 +21,12 @@ DATASET_TRAIN = [
     "data/train/deepset_train.csv"
 ]
 
-MODEL_NAME = "meta-llama/Llama-3.2-1B"
-# MODEL_NAME = "Qwen/Qwen2.5-1.5B-Instruct"
-# MODEL_NAME = "microsoft/Phi-3-mini-128k-instruct"
-
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForCausalLM.from_pretrained(
-    MODEL_NAME, output_attentions=True, return_dict_in_generate=True, attn_implementation="eager", device_map="auto"
-)
+def load_model_and_tokenizer(model_name):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, output_attentions=True, return_dict_in_generate=True, attn_implementation="eager", device_map="auto"
+    )
+    return tokenizer, model
 
 def load_datasets(dataset_paths):
     dataframes = []
@@ -47,7 +45,7 @@ def load_datasets(dataset_paths):
     
     return pd.concat(dataframes, ignore_index=True)
 
-def extract_features(data):
+def extract_features(data, tokenizer, model):
     entropy_scores = []
     variance_scores = []
     for prompt in data['prompt']:
@@ -60,9 +58,7 @@ def extract_features(data):
     data["variance"] = variance_scores
     return data
 
-def random_forest_train(df):
-    df['prompt'] = df['prompt'].apply(preprocess_text)  # call func from utils to preprocess text
-    
+def random_forest_train(df, model_name):    
     # prepare training data
     tfidf = TfidfVectorizer()
     X_tfidf = tfidf.fit_transform(df['prompt'])
@@ -79,15 +75,13 @@ def random_forest_train(df):
     recall = metrics.recall_score(y_test, y_pred)
     f1 = metrics.f1_score(y_test, y_pred)
     accuracy = accuracy_score(y_test, y_pred)
-    print(f"\nRandom Forest metrics on predicting test set on {MODEL_NAME}:")
+    print(f"\nRandom Forest metrics on predicting test set on {model_name}:")
     print(f"Precision: {precision} | Recall: {recall} | F1 Score: {f1}")
     print(f"Accuracy: {accuracy:.2f}")
     print("\nClassification Report:\n", classification_report(y_test, y_pred))
     return rand_forest, tfidf
 
-def log_reg_train(df):
-    df['prompt'] = df['prompt'].apply(preprocess_text)
-
+def log_reg_train(df, model_name):
     tfidf = TfidfVectorizer()
     X_tfidf = tfidf.fit_transform(df['prompt'])
     scaler = StandardScaler()   # scaler for entropy and variance to standardise
@@ -106,15 +100,13 @@ def log_reg_train(df):
     recall = metrics.recall_score(y_test, y_pred)
     f1 = metrics.f1_score(y_test, y_pred)
     accuracy = accuracy_score(y_test, y_pred)
-    print(f"\nLogistic Regression metrics on predicting test set with {MODEL_NAME}:")
+    print(f"\nLogistic Regression metrics on predicting test set with {model_name}:")
     print(f"Precision: {precision} | Recall: {recall} | F1 Score: {f1}")
     print(f"Accuracy: {accuracy:.2f}")
     print("\nClassification Report:\n", classification_report(y_test, y_pred))
     return log_reg, tfidf, scaler
 
-def support_vector_machine(df):
-    df['prompt'] = df['prompt'].apply(preprocess_text)
-
+def support_vector_machine(df, model_name):
     tfidf = TfidfVectorizer()
     X_tfidf = tfidf.fit_transform(df['prompt'])
     scaler = StandardScaler()   # scaler for entropy and variance to standardise
@@ -133,20 +125,32 @@ def support_vector_machine(df):
     recall = metrics.recall_score(y_test, y_pred)
     f1 = metrics.f1_score(y_test, y_pred)
     accuracy = accuracy_score(y_test, y_pred)
-    print(f"\nSupport Vector Machine metrics on predicting test set with {MODEL_NAME}:")
+    print(f"\nSupport Vector Machine metrics on predicting test set with {model_name}:")
     print(f"Precision: {precision} | Recall: {recall} | F1 Score: {f1}")
     print(f"Accuracy: {accuracy:.2f}")
     print("\nClassification Report:\n", classification_report(y_test, y_pred))
     return svm_cls, tfidf, scaler
 
 if __name__ == "__main__":
+    selected_model = "llama"  # change to load saved language models: llama, qwen, phi
+
+    model_options = {
+        "llama": "meta-llama/Llama-3.2-1B",
+        "qwen": "Qwen/Qwen2.5-1.5B-Instruct",
+        "phi": "microsoft/Phi-3-mini-128k-instruct"
+    }
+
+    model_name = model_options[selected_model]
+    tokenizer, model = load_model_and_tokenizer(model_name)
+
     # load all datasets and process them
     load_df = load_datasets(DATASET_TRAIN)
-    df = extract_features(load_df)
+    load_df['prompt'] = load_df['prompt'].apply(preprocess_text)
+    data_extracted = extract_features(load_df, tokenizer, model)
 
-    rand_forest, rand_tfidf = random_forest_train(df)
-    log_reg, log_tfidf, log_scaler = log_reg_train(df)
-    svm_cls, svm_tfidf, svm_scaler = support_vector_machine(df)
+    rand_forest, rand_tfidf = random_forest_train(data_extracted, model_name)
+    log_reg, log_tfidf, log_scaler = log_reg_train(data_extracted, model_name)
+    svm_cls, svm_tfidf, svm_scaler = support_vector_machine(data_extracted, model_name)
     
     # # save random forest model
     # with open('randforest_model.pkl', 'wb') as file:
